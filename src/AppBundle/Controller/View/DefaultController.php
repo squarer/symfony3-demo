1139fa1f859b0c2959c6453ec04a222b22b959b8
squarerliao <squarerliao@gmail.com>
2016-02-09 03:12:50 +0800

    add prefix to api routing

--- src/AppBundle/Controller/View/DefaultController.php
+++ src/AppBundle/Controller/View/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace AppBundle\Controller\View;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
